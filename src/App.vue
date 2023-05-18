82bfe7c167035c1b9214a320055e9a4dec112bd8
Sean James Jr <sjamesjr@brooklynsteamcenter.org>
2023-05-18 14:53:32 -0400

    hi

--- src/App.vue
+++ src/App.vue
@@ -1,115 +1,155 @@
 <script setup>
 
 
-import { ref,watch } from 'vue';
+import { ref, watch } from 'vue';
 
-let todos =ref (JSON.parse(window.localStorage.getItem('todos')))
-let newTodo =ref('')
+let todos = ref(JSON.parse(window.localStorage.getItem('todos')))
+let newTodo = ref('')
+let filter = ref('all')
 
-watch(todos, function(value){
-window.localStorage.setItem('todos',JSON.stringify(value))
-}, {deep: true})
+watch(todos, function (value) {
+  window.localStorage.setItem('todos', JSON.stringify(value))
+}, { deep: true })
 
 function jim() {
-todos.value.push({
-text: newTodo.value,
-complete:false
-})
+  todos.value.push({
+    text: newTodo.value,
+    complete: false
+  })
 
-
-newTodo.value=''
+  newTodo.value = ''
 }
 
 function trash(index) {
-todos.value.splice(index,1)
-
-
+  todos.value.splice(index, 1)
 }
- 
+
+
+
+function todoFilter(todo) {
+  if (filter.value == 'active') {
+    return todo.complete == false
+  } else if (filter.value == 'complete') {
+    return todo.complete == true
+  } else {
+    return true
+  }
+}
+
+function activeFilter(todo) {
+  return todo.complete == false
+}
+
 </script>
 
 <template>
-
   <h1> My Todo Application</h1>
 
+  <p v-if="todos.length > 0">
+  </p>
+
+  <p> {{ todos.filter(activeFilter).length}} items left</p>
+  <p>
+    <input name="filter" type="radio" value="all" v-model="filter">
+    <label>All</label>
+
+    <input name="filter" type="radio" value="active" v-model="filter">
+    <label>Active</label>
+
+    <input name="filter" type="radio" value="complete" v-model="filter">
+    <label>Complete</label>
+
+  </p>
+  <input v-model="newTodo" @keydown.enter="jim" placeholder=" Press enter to add to list">
+  
   <ul>
-  <li v-for="(todo, index) in todos" :class= "{completed: todo.complete}" >
-    <input type="checkbox" v-model="todo.complete">
-    <button @click="trash(index)">🗑️</button>
-   {{ todo.text }}  
-  </li>
+    <li v-for="(todo, index) in todos.filter(todoFilter)" :class="{ completed: todo.complete }">
+      <input type="checkbox" v-model="todo.complete">
+      <button @click="trash(index)">🗑️</button>
+      {{ todo.text }}
+    </li>
   </ul>
-  <input v-model="newTodo" @keydown.enter="jim">
-  <button @click="jim"> Add todo</button>
 
 </template>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap');
 
-li{
+@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
 
-color: white;
-} 
 
-button{
+input{
 
-background-color:grey;
-color: #fffffe;
-text-align: center;
-border-radius: 4px;
-border-style: none;
-font-size: 16px;
-padding: 8px 16px;
-margin: 2px 0;
-cursor: pointer;
+  border-radius: 150px;
+}
+
+li {
+
+  color: black;
+
+}
+
+button {
+
+  background-color: grey;
+  color: black;
+  text-align: center;
+  border-radius: 4px;
+  border-style: none;
+  font-size: 16px;
+  padding: 8px 16px;
+  margin: 2px 0;
+  cursor: pointer;
 }
 
 .center {
-text-align: center;
+  text-align: center;
 }
 
 
 
-h1{
+h1 {
 
-color: #fffffe;
+  color: black;
+  font-family: 'Bebas Neue';
 }
 
-body{
+body {
 
-  background-color: #0f0e17;
+  background-color: white;
 
   text-align: center;
 
-  font-style: Bebas Neue;
+  font-style: 'Bebas Neue';
+
+  color: black;
 }
 
 button:disabled {
-background-color: #f25f4c;
-cursor: not-allowed;
+  background-color: #f25f4c;
+  cursor: not-allowed;
 }
 
 
 
-input[type="checkbox"]{
-appearance: none;
--webkit-appearance: none;
-height: 50px;
-width: 50px;
-background-color: #d5d5d5;
-border-radius: 5px;
-cursor: pointer;
-display: inline-flex;
-align-items: center;
-justify-content: center;
-outline: none;
+input[type="checkbox"] {
+  appearance: none;
+  -webkit-appearance: none;
+  height: 50px;
+  width: 50px;
+  background-color: #d5d5d5;
+  border-radius: 5px;
+  cursor: pointer;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  outline: none;
 }
 
-input[type="checkbox"]::after{
+input[type="checkbox"]::after {
 
   font-family: "Font Awesome 5 Free";
-  content:"\f00c";
+  content: "\f00c";
   font-weight: 900;
   font-size: 50px;
   color: white;
@@ -117,18 +157,16 @@ input[type="checkbox"]::after{
 }
 
 input[type="checkbox"]:hover {
-background-color: #a5a5a5;
+  background-color: #a5a5a5;
 
 }
-input[type='checkbox']:checked{
-background-color: #5bcd3e;
+
+input[type='checkbox']:checked {
+  background-color: #5bcd3e;
 
 }
 
 input[type="checkbox"]:checked::after {
-display:block
-
-
+  display: block
 }
-
 </style>
